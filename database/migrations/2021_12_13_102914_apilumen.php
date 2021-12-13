4acb81a03eef8f0343944a431e907d3ecd6b3d54
dextter1913 <dextter1913@gmail.com>
2021-12-13 11:06:52 -0500

    Primer commit

---
+++ database/migrations/2021_12_13_102914_apilumen.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Apilumen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apilumen', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id')->unsigned();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('document_type', 10);
+            $table->string('document_number', 30);
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->string('phone', 30);
+            $table->timestamps();//crea los cammpos de crear y actualizar automaticamente
+            $table->softDeletes();//crea el campo de eliminacion logica
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
